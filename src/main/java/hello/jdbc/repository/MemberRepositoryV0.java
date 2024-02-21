cc77d23614668bbe788d90d4f740cb5339a5f514
LST <dev.st1027@gmail.com>
2024-02-21 20:48:24 +0900

    test: JDBC로 조회(SELECT) 테스트

--- src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -35,7 +35,7 @@ public class MemberRepositoryV0 {
     }
 
     public Member findById(String memberId) throws SQLException {
-        String sql = "select * from where member_id = ?";
+        String sql = "select * from member where member_id = ?";
 
         Connection con = null;
         PreparedStatement pstmt = null;
--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,11 +1,16 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
@@ -16,5 +21,20 @@ class MemberRepositoryV0Test {
         //save
         Member member = new Member("memberV0", 10000);
         repository.save(member);
+    }
+
+    @DisplayName("JDBC를 활용해 Member 데이터를 조회할 수 있다.")
+    @Test
+    void find() throws SQLException {
+        //given
+        Member member = new Member("memberV1", 10000);
+        repository.save(member);
+
+        //when
+        Member findMember = repository.findById("memberV1");
+        log.info("findMember={}", findMember);
+
+        //then
+        assertThat(findMember).isEqualTo(member);
     }
 }
